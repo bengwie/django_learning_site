eb80338243b2b4070e2e85c8c5111b7ac1cc54f2
Jeffry Hidayat <jhidayat@jhidayat-a01.vmware.com>
2018-09-04 17:19:39 -0700

    Adding step order and 404 exception handling for non-existing courses

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 # Create your views here.
@@ -10,5 +10,5 @@ def course_list(request):
     return render(request, "courses/course_list.html", {"courses" : courses})
 
 def course_detail(request, pk):
-    course = Course.objects.get(pk=pk)
+    course = get_object_or_404(Course, pk=pk)
     return render(request, "courses/course_detail.html", {"course" : course})
